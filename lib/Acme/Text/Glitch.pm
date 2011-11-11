7d5b5818e533967377eda899ef4e02d9bbb13eea
Shinpei Maruyama <shinpeim@gmail.com>
2011-11-11 12:38:48 +0900

    initial commit

---
+++ lib/Acme/Text/Glitch.pm
@@ -0,0 +1,32 @@
+package Acme::Text::Glitch;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Acme::Text::Glitch -
+
+=head1 SYNOPSIS
+
+  use Acme::Text::Glitch;
+
+=head1 DESCRIPTION
+
+Acme::Text::Glitch is
+
+=head1 AUTHOR
+
+Shinpei Maruyama E<lt>shinpeim@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
